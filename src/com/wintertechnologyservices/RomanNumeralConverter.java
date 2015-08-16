f1b11a7ab7a224c0cfd1eb3c7778b5cbdc56a927
Adam Winter <awinter@navmp.com>
2015-08-16 00:05:10 -0400

    refactor logic

--- src/com/wintertechnologyservices/RomanNumeralConverter.java
+++ src/com/wintertechnologyservices/RomanNumeralConverter.java
@@ -1,49 +1,46 @@
 package com.wintertechnologyservices;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class RomanNumeralConverter {
+
+    private static final Map<Integer, String> CONVERSIONS_TO_ROMAN;
+
+    static {
+        CONVERSIONS_TO_ROMAN = new LinkedHashMap<Integer, String>();
+        CONVERSIONS_TO_ROMAN.put(500,"D");
+        CONVERSIONS_TO_ROMAN.put(400,"CD");
+        CONVERSIONS_TO_ROMAN.put(100,"C");
+        CONVERSIONS_TO_ROMAN.put(90,"XC");
+        CONVERSIONS_TO_ROMAN.put(50,"L");
+        CONVERSIONS_TO_ROMAN.put(40,"XL");
+        CONVERSIONS_TO_ROMAN.put(10,"X");
+        CONVERSIONS_TO_ROMAN.put(9,"IX");
+        CONVERSIONS_TO_ROMAN.put(5,"V");
+        CONVERSIONS_TO_ROMAN.put(4,"IV");
+        CONVERSIONS_TO_ROMAN.put(1,"I");
+    }
 
     /** Converts an Arabic number into a Roman numeral */
     public String toRoman (int arabicNumber) {
+
         StringBuilder numeral = new StringBuilder();
 
         // Reduce the input number as the output is being constructed
         while (arabicNumber > 0) {
 
-            if (arabicNumber >= 500) {
-                numeral.append("D");
-                arabicNumber -= 500;
-            } else if (arabicNumber >= 400 ) {
-                numeral.append("CD");
-                arabicNumber -= 400;
-            } if (arabicNumber >= 100) {
-                numeral.append("C");
-                arabicNumber -= 100;
-            } else if (arabicNumber >= 90 ) {
-                numeral.append("XC");
-                arabicNumber -= 90;
-            } if (arabicNumber >= 50) {
-                numeral.append("L");
-                arabicNumber -= 50;
-            } else if (arabicNumber >= 40 ) {
-                numeral.append("XL");
-                arabicNumber -= 40;
-            } else if (arabicNumber >= 10) {
-                numeral.append("X");
-                arabicNumber -= 10;
-            } else if (arabicNumber >= 9 ) {
-                numeral.append("IX");
-                arabicNumber -= 9;
-            } else if (arabicNumber >= 5) {
-                numeral.append("V");
-                arabicNumber -= 5;
-            } else if (arabicNumber >= 4 ) {
-                numeral.append("IV");
-                arabicNumber -= 4;
-            } else if (arabicNumber > 0 ) {
-                numeral.append("I");
-                arabicNumber -= 1;
-            }
+            // Iterate through the conversion map
+            for (Map.Entry<Integer, String> entry : CONVERSIONS_TO_ROMAN.entrySet()) {
+                if (arabicNumber >= entry.getKey()) {
+                    numeral.append(entry.getValue());
+                    arabicNumber -= entry.getKey();
 
+                    // found a matching character conversion, break out of for loop
+                    break;
+                };
+            }
         }
 
         return numeral.toString();
