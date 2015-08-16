f7d7a083cc8c28f3df36aa63a27845ec77fdaadc
Adam Winter <awinter@navmp.com>
2015-08-15 22:46:28 -0400

    refactor tests to add a set up method

--- test/com/wintertechnologyservices/RomanNumeralConverterTest.java
+++ test/com/wintertechnologyservices/RomanNumeralConverterTest.java
@@ -1,26 +1,31 @@
 package com.wintertechnologyservices;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class RomanNumeralConverterTest {
 
+    private RomanNumeralConverter converter;
+
+    @Before
+    public void setUp() {
+        converter = new RomanNumeralConverter();
+    }
+
     @Test
     public void convertArabicNumber1 () {
-        RomanNumeralConverter converter = new RomanNumeralConverter();
         assertEquals("I", converter.toRoman(1));
     }
 
     @Test
     public void convertArabicNumber3 () {
-        RomanNumeralConverter converter = new RomanNumeralConverter();
         assertEquals("III", converter.toRoman(3));
     }
 
     @Test
     public void convertArabicNumber5 () {
-        RomanNumeralConverter converter = new RomanNumeralConverter();
         assertEquals("V", converter.toRoman(5));
     }
 }
